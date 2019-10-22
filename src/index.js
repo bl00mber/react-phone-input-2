8f80a0ceb1aa0509959e5e15eb2c379671dcd840
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2019-10-22 09:30:25 -0600

    setting defauldProp for isDropdownShowing

--- src/index.js
+++ src/index.js
@@ -110,7 +110,8 @@ class ReactPhoneInput extends React.Component {
     keys: {
       UP: 38, DOWN: 40, RIGHT: 39, LEFT: 37, ENTER: 13,
       ESC: 27, PLUS: 43, A: 65, Z: 90, SPACE: 32
-    }
+    },
+    isDropdownShowing: () => null,
   }
 
   constructor(props) {
