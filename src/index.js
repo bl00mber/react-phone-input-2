fcac70accaec35fdaa2dd3beca2143f16a4c5786
Tor Andreas Johnsen <andru_joh@yahoo.no>
2019-12-01 17:30:59 +0100

    Choose first item in array if none highlighted (#140)

--- src/index.js
+++ src/index.js
@@ -601,7 +601,7 @@ class PhoneInput extends React.Component {
         break;
       case keys.ENTER:
         if (this.props.enableSearchField) {
-          this.handleFlagItemClick(this.getSearchFilteredCountries()[this.state.highlightCountryIndex], e);
+          this.handleFlagItemClick(this.getSearchFilteredCountries()[this.state.highlightCountryIndex] || this.getSearchFilteredCountries()[0], e);
         } else {
           this.handleFlagItemClick([...this.state.preferredCountries, ...this.state.onlyCountries][this.state.highlightCountryIndex], e);
         }
