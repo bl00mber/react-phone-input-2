f0707eb63dad3c00d133ba29e788655768c87a6b
David Barbora <david.barbora@vacuumlabs.com>
2022-01-27 19:01:00 +0100

    search by country code with plus sign

--- src/index.js
+++ src/index.js
@@ -764,7 +764,7 @@ class PhoneInput extends React.Component {
     const { preferredCountries, onlyCountries, searchValue } = this.state
     const { enableSearch } = this.props
     const allCountries = preferredCountries.concat(onlyCountries);
-    const sanitizedSearchValue = searchValue.trim().toLowerCase();
+    const sanitizedSearchValue = searchValue.trim().toLowerCase().replace('+','');
     if (enableSearch && sanitizedSearchValue) {
       // [...new Set()] to get rid of duplicates
       // firstly search by iso2 code
