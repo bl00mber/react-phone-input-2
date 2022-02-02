2810bada3f9353d6ca12deb888b54e7e5453d765
DylanCulfogienis <65580873+DylanCulfogienis@users.noreply.github.com>
2022-02-02 10:27:56 -0700

    Input ref fix (#187) (#449)

--- src/index.js
+++ src/index.js
@@ -950,12 +950,19 @@ class PhoneInput extends React.Component {
           onBlur={this.handleInputBlur}
           onCopy={this.handleInputCopy}
           value={formattedNumber}
-          ref={el => this.numberInputRef = el}
           onKeyDown={this.handleInputKeyDown}
           placeholder={this.props.placeholder}
           disabled={this.props.disabled}
           type='tel'
           {...this.props.inputProps}
+          ref={el => {
+            this.numberInputRef = el;
+            if (typeof this.props.inputProps.ref === 'function') {
+              this.props.inputProps.ref(el);
+            } else if (typeof this.props.inputProps.ref === 'object') {
+              this.props.inputProps.ref.current = el;
+            }
+          }}
         />
 
         <div
